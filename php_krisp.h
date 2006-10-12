b136e5c7e9636c4c71afcb1886c4fc7ed84c6ed1
JoungKyun Kim <joungkyun@daum.net>
2006-10-12 17:08:39 +0000

    start 1.1.3

--- php_krisp.h
+++ php_krisp.h
@@ -15,7 +15,7 @@
   | Author: JoungKyun.Kim <http://www.oops.org>                          |
   +----------------------------------------------------------------------+
 
-  $Id: php_krisp.h,v 1.8 2006-09-14 08:53:33 oops Exp $
+  $Id: php_krisp.h,v 1.9 2006-10-12 17:08:39 oops Exp $
 */
 
 #ifndef PHP_KRISP_H
@@ -74,7 +74,7 @@ ZEND_END_MODULE_GLOBALS(krisp)
 #define KRISP_G(v) (krisp_globals.v)
 #endif
 
-#define BUILDNO "1.1.2"
+#define BUILDNO "1.1.3"
 
 #define phpext_krisp_ptr krisp_module_ptr
 
