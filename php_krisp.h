47b1b4128156d5750dc6153a842a7d81cf4d096a
JoungKyun Kim <joungkyun@daum.net>
2010-08-08 16:40:58 +0000

    fix license to LGPL current and old versions

--- php_krisp.h
+++ php_krisp.h
@@ -1,21 +1,5 @@
 /*
-  +----------------------------------------------------------------------+
-  | PHP Version 5                                                        |
-  +----------------------------------------------------------------------+
-  | Copyright (c) 1997-2006 The PHP Group                                |
-  +----------------------------------------------------------------------+
-  | This source file is subject to version 3.0 of the PHP license,       |
-  | that is bundled with this package in the file LICENSE, and is        |
-  | available at through the world-wide-web at                           |
-  | http://www.php.net/license/3_0.txt.                                  |
-  | If you did not receive a copy of the PHP license and are unable to   |
-  | obtain it through the world-wide-web, please send a note to          |
-  | license@php.net so we can mail you a copy immediately.               |
-  +----------------------------------------------------------------------+
-  | Author: JoungKyun.Kim <http://www.oops.org>                          |
-  +----------------------------------------------------------------------+
-
-  $Id: php_krisp.h,v 1.18 2010-08-08 16:05:32 oops Exp $
+ * $Id: php_krisp.h,v 1.19 2010-08-08 16:40:58 oops Exp $
 */
 
 #ifndef PHP_KRISP_H
