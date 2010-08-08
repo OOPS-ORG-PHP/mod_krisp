d785fa4a37c8940551e7ae6a9211900ca8391afe
oops <oops@f9a68ef8-65cc-11e0-8aaf-e44620aa2436>
2010-08-08 16:33:01 +0000

    fixed lincense to LGPL

--- trunk/php_krisp.c
+++ trunk/php_krisp.c
@@ -2,7 +2,7 @@
   +----------------------------------------------------------------------+
   | PHP Version 5                                                        |
   +----------------------------------------------------------------------+
-  | Copyright (c) 1997-2006 The PHP Group                                |
+  | Copyright (c) 2010 JoungKyun.Kim                                     |
   +----------------------------------------------------------------------+
   | This source file is subject to version 3.0 of the PHP license,       |
   | that is bundled with this package in the file LICENSE, and is        |
@@ -15,7 +15,7 @@
   | Author: JoungKyun.Kim <http://oops.org>                              |
   +----------------------------------------------------------------------+
 
-  $Id: php_krisp.c,v 1.16 2010-08-08 16:03:40 oops Exp $
+  $Id: php_krisp.c,v 1.17 2010-08-08 16:33:01 oops Exp $
 */
 
 /*
