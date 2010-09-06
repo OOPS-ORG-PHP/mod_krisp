a7dcd1820de11d9ac0f2fa6627af674749052747
nobody <nobody@f9a68ef8-65cc-11e0-8aaf-e44620aa2436>
2010-09-06 05:47:01 +0000

    This commit was manufactured by cvs2svn to create tag 'v2_0_2'.

---
+++ tags/v2_0_2/php_krisp.c
@@ -0,0 +1,482 @@
+/**
+ * Project: mod_krisp :: KRISP database php extension
+ * File:    krisp.c
+ *
+ * Copyright (c) 1997-2010 JoungKyun.Kim
+ *
+ * LICENSE: LGPL
+ *
+ * @category    Database
+ * @package     mod_krisp
+ * @author      JoungKyun.Kim <http://oops.org>
+ * @copyright   1997-2010 OOPS.org
+ * @license     LGPL
+ * @version     CVS: $Id: php_krisp.c,v 1.22 2010-09-06 05:36:24 oops Exp $
+ * @link        http://pear.oops.org/package/krisp
+ * @since       File available since release 0.0.1
+ */
+
+/*
+ * PHP5 Korea IPS library module "krsip"
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "php.h"
+#include "php_ini.h"
+#include "SAPI.h"
+#include "ext/standard/info.h"
+
+#include "php_krisp.h"
+
+/* If you declare any globals in php_krisp.h uncomment this:
+ZEND_DECLARE_MODULE_GLOBALS(krisp)
+ */
+
+/* True global resources - no need for thread safety here */
+static int le_krisp;
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_krisp_open, 0, 0, 0)
+	ZEND_ARG_INFO(0, database)
+	ZEND_ARG_INFO(1, error)
+ZEND_END_ARG_INFO()
+
+/* {{{ krisp_functions[]
+ *
+ * Every user visible function must have an entry in krisp_functions[].
+ */
+function_entry krisp_functions[] = {
+	PHP_FE(krisp_buildver,			NULL)
+	PHP_FE(krisp_version,			NULL)
+	PHP_FE(krisp_uversion,			NULL)
+	PHP_FE(krisp_open,				arginfo_krisp_open)
+	PHP_FE(krisp_search,			NULL)
+	PHP_FE(krisp_search_ex,			NULL)
+	PHP_FE(krisp_close,				NULL)
+	PHP_FE(krisp_netmask,			NULL)
+	PHP_FE(krisp_network,			NULL)
+	PHP_FE(krisp_broadcast,			NULL)
+	PHP_FE(krisp_prefix2mask,		NULL)
+	PHP_FE(krisp_mask2prefix,		NULL)
+	{NULL, NULL, NULL}
+};
+/* }}} */
+
+/* {{{ krisp_module_entry
+ */
+zend_module_entry krisp_module_entry = {
+#if ZEND_MODULE_API_NO >= 20010901
+	STANDARD_MODULE_HEADER,
+#endif
+	"krisp",
+	krisp_functions,
+	PHP_MINIT(krisp),
+	NULL,
+	NULL,
+	NULL,
+	PHP_MINFO(krisp),
+#if ZEND_MODULE_API_NO >= 20010901
+	BUILDNO, /* Replace with version number for your extension */
+#endif
+	STANDARD_MODULE_PROPERTIES
+};
+/* }}} */
+
+#ifdef COMPILE_DL_KRISP
+ZEND_GET_MODULE(krisp)
+#endif
+
+static void _close_krisp_link(zend_rsrc_list_entry *rsrc TSRMLS_DC)
+{
+	KRISP_API *kr = (KRISP_API *)rsrc->ptr;
+	kr_close (kr->db);
+	free (kr);
+}
+
+/* {{{ PHP_MINIT_FUNCTION
+ */
+PHP_MINIT_FUNCTION(krisp)
+{
+	le_krisp = zend_register_list_destructors_ex(_close_krisp_link, NULL, "krisp link", module_number);
+	return SUCCESS;
+}
+/* }}} */
+
+/* {{{ PHP_MINFO_FUNCTION
+ */
+PHP_MINFO_FUNCTION(krisp)
+{
+	php_info_print_table_start();
+	php_info_print_table_colspan_header(2, "krisp extension support");
+	php_info_print_table_row(2, "Summary", "Get Korea ISP Information");
+	php_info_print_table_row(2, "URL", "http://devel.oops.org/");
+	php_info_print_table_row(2, "Build version", BUILDNO);
+	php_info_print_table_row(2, "Library version", KRISP_VERSION);
+	php_info_print_table_colspan_header(2, "Library support");
+	php_info_print_table_row(2, "SQLite", SQLITE_VERSION);
+	php_info_print_table_end();
+}
+/* }}} */
+
+/* {{{ proto string krisp_buildver (void)
+ *  print krisp extension build number */
+PHP_FUNCTION(krisp_buildver)
+{
+	RETURN_STRING (BUILDNO, 1);
+}
+/* }}} */
+
+/* {{{ proto string krisp_version (void)
+ *  print krisp library version */
+PHP_FUNCTION(krisp_version)
+{
+	RETURN_STRING (KRISP_VERSION, 1);
+}
+/* }}} */
+
+/* {{{ proto string krisp_uversion (void)
+ *  print krisp library uversion */
+PHP_FUNCTION(krisp_uversion)
+{
+	RETURN_STRING (KRISP_UVERSION, 1);
+}
+/* }}} */
+
+/* {{{ proto resource krisp_open (char datafile)
+ *  return krisp database open resource */
+PHP_FUNCTION(krisp_open)
+{
+	/*
+	zval **datafile = NULL;
+	zval **error = NULL;
+	*/
+	char *database = NULL;
+	int database_len;
+	zval *error = NULL;
+	KRISP_API *kr;
+
+	char err[1024];
+
+	if ( zend_parse_parameters (ZEND_NUM_ARGS () TSRMLS_CC, "|sz", &database, &database_len, &error) == FAILURE )
+		return;
+
+	if ( database != NULL && database_len < 1 )
+		database = NULL;
+
+	kr = (KRISP_API *) malloc (sizeof (KRISP_API));
+
+	if ( kr_open_safe (&kr->db, database, err) == false ) {
+		php_error_docref (NULL TSRMLS_CC, E_WARNING, "%s", err);
+		if ( error != NULL ) {
+			zval_dtor (error);
+			ZVAL_STRING (error, err, 1);
+		}
+		free (kr);
+		RETURN_FALSE;
+	}
+
+	kr->rsrc = ZEND_REGISTER_RESOURCE(return_value, kr, le_krisp);
+}
+/* }}} */
+
+/* {{{ proto object krisp_search (resource, char *host)
+ *  return isp information array */
+PHP_FUNCTION(krisp_search)
+{
+	zval *krisp_link;
+	char *host;
+	int host_len;
+	KRISP_API *kr;
+
+	KRNET_API isp;
+	char rip[16];
+	ulong networkv;
+	ulong broadcastv;
+
+	if ( zend_parse_parameters (ZEND_NUM_ARGS () TSRMLS_CC, "rs", &krisp_link, &host, &host_len) == FAILURE)
+		return;
+
+	if ( host_len == 0) {
+		php_error_docref (NULL TSRMLS_CC, E_WARNING, "length of host argument is 0");
+		RETURN_FALSE;
+	}
+
+	SAFECPY_256 (isp.ip, host);
+	isp.verbose = false;
+
+	ZEND_FETCH_RESOURCE (kr, KRISP_API *, &krisp_link, -1, "KRISP database", le_krisp);
+
+	if ( kr_search (&isp, kr->db) ) {
+		php_error_docref (NULL TSRMLS_CC, E_WARNING, "%s", isp.err);
+		RETURN_FALSE;
+	}
+
+	if ( object_init (return_value) == FAILURE ) {
+		php_error_docref (NULL TSRMLS_CC, E_WARNING, "Failure object initialize");
+		RETURN_FALSE;
+	}
+
+	networkv = network (isp.start, isp.netmask);
+	broadcastv = broadcast (isp.start, isp.netmask);
+
+	add_property_string (return_value, "host", host, 1);
+	add_property_string (return_value, "ip", isp.ip, 1);
+	add_property_string (return_value, "start", long2ip_r (isp.start, rip), 1);
+	add_property_string (return_value, "end", long2ip_r (isp.end, rip), 1);
+	add_property_string (return_value, "netmask", long2ip_r (isp.netmask, rip), 1);
+	add_property_string (return_value, "network", long2ip_r (networkv, rip), 1);
+	add_property_string (return_value, "broadcast", long2ip_r (broadcastv, rip), 1);
+	add_property_string (return_value, "icode", isp.icode, 1);
+	add_property_string (return_value, "iname", isp.iname, 1);
+	add_property_string (return_value, "ccode", isp.ccode, 1);
+	add_property_string (return_value, "cname", isp.cname, 1);
+}
+/* }}} */
+
+/* {{{ proto object krisp_search_ex (resource, char *host)
+ *  return isp information array */
+PHP_FUNCTION(krisp_search_ex)
+{
+	zval *krisp_link;
+	char *host;
+	int host_len;
+	KRISP_API *kr;
+
+	KRNET_API_EX isp;
+	char rip[16];
+	zval *dummy = NULL;
+	char *table;
+	int table_len;
+	ulong netmask;
+	ulong networkv;
+	ulong broadcastv;
+
+	if ( zend_parse_parameters (ZEND_NUM_ARGS () TSRMLS_CC, "rs|s", &krisp_link, &host, &host_len, &table, &table_len) == FAILURE )
+		return;
+
+	if ( ZEND_NUM_ARGS () == 2 ) {
+		table = "krisp";
+		table_len = 5;
+	}
+
+	if ( table_len == 0 )
+		table = "krisp";
+
+	if ( strlen (host) == 0) {
+		php_error_docref (NULL TSRMLS_CC, E_WARNING, "length of host argument is 0");
+		RETURN_FALSE;
+	}
+
+	SAFECPY_256 (isp.ip, host);
+	isp.verbose = false;
+
+	ZEND_FETCH_RESOURCE (kr, KRISP_API *, &krisp_link, -1, "KRISP database", le_krisp);
+	kr->db->table = table;
+
+	if ( kr_search_ex (&isp, kr->db) ) {
+		php_error_docref (NULL TSRMLS_CC, E_WARNING, "%s", isp.err);
+		initStruct_ex (&isp, true);
+		RETURN_FALSE;
+	}
+
+	if ( object_init (return_value) == FAILURE ) {
+		initStruct_ex (&isp, true);
+		php_error_docref (NULL TSRMLS_CC, E_WARNING, "Failure object initialize");
+		RETURN_FALSE;
+	}
+
+	MAKE_STD_ZVAL(dummy);
+	if ( array_init (dummy) == FAILURE ) {
+		initStruct_ex (&isp, true);
+		php_error_docref (NULL TSRMLS_CC, E_WARNING, "Failure array initialize");
+		RETURN_FALSE;
+	}
+
+	netmask = guess_netmask (isp.start, isp.end);
+	networkv = network (isp.start, netmask);
+	broadcastv = broadcast (isp.start, netmask);
+
+	add_property_string (return_value, "host", host, 1);
+	add_property_string (return_value, "ip", isp.ip, 1);
+	add_property_string (return_value, "start", long2ip_r (isp.start, rip), 1);
+	add_property_string (return_value, "end", long2ip_r (isp.end, rip), 1);
+	add_property_string (return_value, "netmask", long2ip_r (netmask, rip), 1);
+	add_property_string (return_value, "network", long2ip_r (networkv, rip), 1);
+	add_property_string (return_value, "broadcast", long2ip_r (broadcastv, rip), 1);
+	add_property_long (return_value, "size", isp.size);
+
+	{
+		short r;
+
+		for ( r=0; r<isp.size; r++ )
+			add_index_string (dummy, r, isp.dummy[r], 1);
+	}
+
+	add_property_zval (return_value, "data", dummy);
+	initStruct_ex (&isp, true);
+}
+/* }}} */
+
+/* {{{ proto void krisp_close (resource)
+ *  close krisp database */
+PHP_FUNCTION(krisp_close)
+{
+	zval *krisp_link;
+	KRISP_API *kr;
+
+	if ( zend_parse_parameters (ZEND_NUM_ARGS () TSRMLS_CC, "r", &krisp_link) == FAILURE )
+		return;
+
+	ZEND_FETCH_RESOURCE (kr, KRISP_API *, &krisp_link, -1, "KRISP database", le_krisp);
+
+	zend_list_delete(Z_RESVAL_P(krisp_link));
+}
+/* }}} */
+
+/* {{{ proto object krisp_netmask (start, end)
+ *  return netmask and prefix about given ip range */
+PHP_FUNCTION(krisp_netmask)
+{
+	char rip[16];
+	char *start;
+	int start_len;
+	char *end;
+	char end_len;
+	ulong lstart;
+	ulong lend;
+	ulong mask;
+
+	if ( zend_parse_parameters (ZEND_NUM_ARGS () TSRMLS_CC, "ss", &start, &start_len, &end, &end_len) == FAILURE )
+		return;
+
+	if ( start_len == 0 ) {
+		php_error_docref (NULL TSRMLS_CC, E_WARNING, "start address is none");
+		RETURN_FALSE;
+	}
+
+	if ( end_len == 0 ) {
+		php_error_docref (NULL TSRMLS_CC, E_WARNING, "end address is none");
+		RETURN_FALSE;
+	}
+
+	if ( strchr (start, '.') == NULL )
+		lstart = (ulong) strtoul (start, NULL, 10);
+	else
+		lstart = ip2long (start);
+
+	if ( strchr (end, '.') == NULL )
+		lend = (ulong) strtoul (end, NULL, 10);
+	else
+		lend = ip2long (end);
+
+	if ( object_init (return_value) == FAILURE ) {
+		php_error_docref (NULL TSRMLS_CC, E_WARNING, "Failure object initialize");
+		RETURN_FALSE;
+	}
+
+	mask = guess_netmask (lstart, lend);
+	add_property_string (return_value, "mask", long2ip_r (mask, rip), 1);
+	add_property_long (return_value, "prefix", long2prefix (mask));
+}
+/* }}} */
+
+static void krisp_network_broadcast (INTERNAL_FUNCTION_PARAMETERS, zend_bool type) // {{{
+{
+	char *ip;
+	int ip_len;
+	char *mask;
+	int mask_len;
+	ulong lip;
+	ulong lmask;
+	char rip[16];
+
+	if ( zend_parse_parameters (ZEND_NUM_ARGS () TSRMLS_CC, "ss", &ip, &ip_len, &mask, &mask_len) == FAILURE )
+		return;
+
+	if ( ip_len == 0) {
+		php_error_docref (NULL TSRMLS_CC, E_WARNING, "First argument (IP address) is none");
+		RETURN_FALSE;
+	}
+
+	if ( mask_len == 0) {
+		php_error_docref (NULL TSRMLS_CC, E_WARNING, "Second argument (Network mask) is none");
+		RETURN_FALSE;
+	}
+
+	if ( strchr (ip, '.') == NULL )
+		lip = (ulong) strtoul (ip, NULL, 10);
+	else
+		lip = ip2long (ip);
+
+	if ( strchr (mask, '.') == NULL )
+		lmask = (ulong) strtoul (mask, NULL, 10);
+	else
+		lmask = ip2long (mask);
+
+	if ( type ) {
+		RETURN_STRING (long2ip_r (broadcast (lip, lmask), rip), 1);
+	} else {
+		RETURN_STRING (long2ip_r (network (lip, lmask), rip), 1);
+	}
+} // }}}
+
+/* {{{ proto string krisp_network (ip, mask)
+ *  reuturn network address about given ip and network mask */
+PHP_FUNCTION(krisp_network)
+{
+	krisp_network_broadcast (INTERNAL_FUNCTION_PARAM_PASSTHRU, 0);
+}
+/* }}} */
+
+/* {{{ proto string krisp_broadcast (ip, mask)
+ *  reuturn broadcast address about given ip and network mask */
+PHP_FUNCTION(krisp_broadcast)
+{
+	krisp_network_broadcast (INTERNAL_FUNCTION_PARAM_PASSTHRU, 1);
+}
+/* }}} */
+
+/* {{{ proto string krisp_prefix2mask (prefix)
+ *  return unsigned long value for given network prefix */
+PHP_FUNCTION(krisp_prefix2mask)
+{
+	short prefix;
+	char rip[16];
+
+	if ( zend_parse_parameters (ZEND_NUM_ARGS () TSRMLS_CC, "l", &prefix) == FAILURE )
+		return;
+
+	RETURN_STRING (long2ip_r (prefix2long (prefix), rip), 1);
+}
+/* }}} */
+
+/* {{{ proto short krisp_mask2prefix (mask)
+ *  return short network prefix for given long network mask */
+PHP_FUNCTION(krisp_mask2prefix)
+{
+	char * mask;
+	int mask_len;
+
+	if ( zend_parse_parameters (ZEND_NUM_ARGS () TSRMLS_CC, "s", &mask, &mask_len) == FAILURE )
+		return;
+
+	RETURN_LONG (long2prefix (ip2long (mask)));
+}
+/* }}} */
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */
---
+++ tags/v2_0_2/php_krisp.h
@@ -0,0 +1,93 @@
+/*
+ * $Id: php_krisp.h,v 1.21 2010-09-06 05:47:00 oops Exp $
+*/
+
+#ifndef PHP_KRISP_H
+#define PHP_KRISP_H
+
+#ifdef PHP_WIN32
+#define PHP_KRISP_API __declspec(dllexport)
+#else
+#define PHP_KRISP_API
+#endif
+
+#ifdef ZTS
+#include "TSRM.h"
+#endif
+
+extern zend_module_entry krisp_module_entry;
+#define krisp_module_ptr &krisp_module_entry
+
+PHP_MINIT_FUNCTION(krisp);
+PHP_MSHUTDOWN_FUNCTION(krisp);
+PHP_RINIT_FUNCTION(krisp);
+PHP_RSHUTDOWN_FUNCTION(krisp);
+PHP_MINFO_FUNCTION(krisp);
+
+PHP_FUNCTION(krisp_buildver);
+PHP_FUNCTION(krisp_version);
+PHP_FUNCTION(krisp_uversion);
+PHP_FUNCTION(krisp_open);
+PHP_FUNCTION(krisp_close);
+PHP_FUNCTION(krisp_search);
+PHP_FUNCTION(krisp_search_ex);
+PHP_FUNCTION(krisp_netmask);
+PHP_FUNCTION(krisp_network);
+PHP_FUNCTION(krisp_broadcast);
+PHP_FUNCTION(krisp_prefix2mask);
+PHP_FUNCTION(krisp_mask2prefix);
+
+/* 
+  	Declare any global variables you may need between the BEGIN
+	and END macros here:     
+ */
+
+/*
+ZEND_BEGIN_MODULE_GLOBALS(krisp)
+	char err[1024];
+ZEND_END_MODULE_GLOBALS(krisp)
+*/
+
+/* In every utility function you add that needs to use variables 
+   in php_krisp_globals, call TSRM_FETCH(); after declaring other 
+   variables used by that function, or better yet, pass in TSRMG_CC
+   after the last function argument and declare your utility function
+   with TSRMG_DC after the last declared argument.  Always refer to
+   the globals in your function as KRISP_G(variable).  You are 
+   encouraged to rename these macros something shorter, see
+   examples in any other php module directory.
+*/
+
+#ifdef ZTS
+#define KRISP_G(v) TSRMG(krisp_globals_id, zend_krisp_globals *, v)
+#else
+#define KRISP_G(v) (krisp_globals.v)
+#endif
+
+#define BUILDNO "2.0.2"
+
+#define phpext_krisp_ptr krisp_module_ptr
+
+
+/*
+ * KRISP library header
+ */
+
+#include <ipcalc.h>
+#include <krisp.h>
+
+typedef struct krisp_info {
+	KR_API *db;
+	int rsrc;
+} KRISP_API;
+
+#endif	/* PHP_KRISP_H */
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */
