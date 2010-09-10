037ad3791898e05e648681ebdfd9976eb75d70fa
nobody <nobody@f9a68ef8-65cc-11e0-8aaf-e44620aa2436>
2010-09-10 20:01:33 +0000

    This commit was manufactured by cvs2svn to create tag 'v2_1_0'.

---
+++ tags/v2_1_0/bench.php
@@ -0,0 +1,42 @@
+#!/usr/bin/php
+<?
+/* $Id: bench.php,v 1.3 2010-09-10 18:10:39 oops Exp $ */
+
+if ( ! extension_loaded ('krisp') )
+	dl ('krisp.so');
+
+function get_microtime($old, $new) {
+	$start = explode(" ", $old);
+	$end = explode(" ", $new);
+
+	return sprintf("%.2f", ($end[1] + $end[0]) - ($start[1] + $start[0]));
+}
+
+function random_ip () {
+	return (mt_rand (8388608, 2147483647) * 2);
+}
+
+if ( $argc != 2 || $argv[1] == '-h' ) {
+	error_log ("Usage: {$argv[0]} query_limit");
+	exit (1);
+}
+
+$c = krisp_open ();
+# don't check database mtime
+krisp_set_mtime_interval ($c, 0);
+
+$t1 = microtime ();
+
+for ( $i=0; $i<$argv[1]; $i++ ) {
+	$v = random_ip ();
+	$v = long2ip ($v);
+	$r = krisp_search_ex ($c, $v);
+	#$r = krisp_search_ex ($c, $v, 'krisp');
+}
+
+$t2 = microtime ();
+
+krisp_close ($c);
+
+printf ("Execute time: %f sec\n", get_microtime ($t1, $t2));
+?>
